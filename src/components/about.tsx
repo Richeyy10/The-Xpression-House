5aa4b5491e42d0fc1f3e3f81d430888901b19657
righteousness <righteousness@righteousness-MacBook-Air.local>
2026-01-15 12:55:10 +0100

    Events and About sorted

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,6 +1,15 @@
+'use client';
 import localFont from "next/font/local";
 import Image from "next/image";
-import about from "@/assets/images/about.jpg"
+import img1 from "../assets/images/img_1.jpg";
+import img2 from "../assets/images/img_2.jpg";
+import img3 from "../assets/images/img_3.jpg";
+import img4 from "../assets/images/img_4.jpg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 
 //Local hosted Fonts
 const happyTimes = localFont({
@@ -20,10 +29,10 @@ const geoslabBold = localFont({
 })
 
 export default function About() {
-    return(
+    return (
         <>
             <div id="about" className="w-full md:w-[90%] mx-auto md:flex">
-                <div className="block md:w-[50%] text-center pt-[8%]">
+                <div className="flex flex-col items-center justify-center md:w-[50%] text-center">
                     <h1 className={`text-white text-center text-xl sm:text-3xl font-bold ${geoslab.className}`}>About Xpression House</h1>
                     <p className={`text-white text-sm sm:text-lg text-justify w-[70%] pt-[30px] mx-auto sm:leading-10 ${poppins.className}`} >Xpression House is the youth church of The Stone Church (Word Alive Ministries Int&apos;l). Since 2009, we&apos;ve been a body of young and vibrant believers, students and professionals passionate about the Gospel of the Kingdom and living out the culture  of heaven on earth. Guided by the Bible as our foundation, our mandate goes beyond the four walls of the church to impact lives and shape destinies.</p>
                     <button className={`font-happy-times my-[20px] text-1xl rounded-3xl bg-[#37C500] text-white w-[100px] sm:w-[150px] h-[40px] sm:h-[50px] hover:bg-white hover:text-[#37C500] ${poppins.className}`}>Read More</button>
@@ -31,7 +40,26 @@ export default function About() {
                 {/* <div className="relative h-[300px] mt-[150px] sm:h-[600px] mr-[30px] sm:mt-[50px] sm:mr-[150px] bg-cover bg-center block w-[50%]" style={{ backgroundImage: `url(${about.src})` }}>
                 </div> */}
                 <div className="w-[70%] mx-auto md:w-[50%] flex items-center justify-center md:pt-[60px] pb-[60px]">
-                    <Image src={about} alt="About Us" width={500} />
+                    <div className="w-full h-full">
+                        <Swiper
+                            modules={[Pagination, Autoplay]}
+                            spaceBetween={16}
+                            slidesPerView={1}
+                            pagination={{ clickable: true }}
+                            autoplay={{ delay: 3500, disableOnInteraction: false }}
+                            loop
+                            className="h-[560px] w-[550px] rounded-3xl"
+                            aria-live="polite"
+                        >
+                            {[img1, img2, img3, img4].map((img, idx) => (
+                                <SwiperSlide key={idx} className="h-90">
+                                    <div className="h-90 w-full">
+                                        <Image src={img} alt={`Impact ${idx + 1}`} className="object-cover h-[580px] w-[550px]" />
+                                    </div>
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
+                    </div>
                 </div>
             </div>
         </>
--- src/components/events.tsx
+++ src/components/events.tsx
@@ -1,6 +1,13 @@
+'use client';
 import Image from "next/image";
 import React from "react";
-import ablaze from "@/assets/images/ablaze.jpg"
+import communionsunday from "../assets/images/Communion Sunday.jpg";
+import encounternight from "../assets/images/Encounter Night.jpg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import localFont from "next/font/local";
 
 //Local hosted Fonts
@@ -24,17 +31,39 @@ const geoslabBold = localFont({
 export default function Event() {
     return (
         <div id="about" className="w-[90%] mx-auto mb-[60px] block md:flex">
-            <div className="block md:w-[50%] text-center sm:pt-[5%]">
+            <div className="flex flex-col items-center justify-center md:w-[50%] text-center">
                 <h1 className={`text-white text-center text-xl sm:text-3xl font-bold ${geoslab.className}`}>Upcoming Events</h1>
-                <p className={`text-white text-sm sm:text-lg text-justify w-[70%] pt-[20px] md:pt-[30px] mx-auto sm:leading-10 ${poppins.className}`}>Ablaze Conference is the annual convention of the Xpression House, a powerful retreat where believers gather to be rekindled in the Word and refreshed in God&apos;s presence.</p>
-                <p className={`text-white text-sm sm:text-lg text-justify w-[70%] md:pt-[0px] mx-auto sm:leading-10 ${poppins.className}`}>From October 8th to 12th 2025, hearts will be set ablaze under the theme DOMINION ACTIVATED.</p>
-                <p className={`text-white text-sm sm:text-lg text-justify w-[70%] md:pt-[0px] mx-auto sm:leading-10 ${poppins.className}`}>An encounter that ignites purpose, Ablaze is for you!</p>
-                <button className={`font-happy-times my-[20px] text-1xl rounded-3xl bg-[#37C500] text-white w-[100px] sm:w-[150px] h-[40px] sm:h-[50px] hover:bg-white hover:text-[#37C500] ${poppins.className}`}>Read More</button>
+                <h1 className={`text-white text-center text-xl sm:text-3xl font-bold ${poppins.className}`}>Join Our Services and Programs</h1>
+                <p className={`text-white text-sm sm:text-lg text-center w-[70%] pt-[20px] md:pt-[30px] mx-auto sm:leading-10 ${poppins.className}`}>Find out God&apos;s plans for your life.</p>
+                <div className="flex justify-center my-8 gap-4">
+                    <button className={`font-happy-times my-[20px] text-1xl rounded-3xl bg-[#37C500] text-white w-[100px] sm:w-[150px] h-[40px] sm:h-[50px] hover:bg-white hover:text-[#37C500] ${poppins.className}`}>Livestream</button>
+                    <button className={`my-[20px] text-1xl rounded-3xl bg-transparent border-2 text-white w-[120px] sm:w-[150px] h-[35px] sm:h-[50px] hover:bg-white hover:text-[#37C500] ${poppins.className}`}>View Events</button>
+                </div>
             </div>
             {/* <div className="relative h-[200px] mt-[70px] sm:mt-[10%] mr-[30px] sm:mr-[150px] bg-cover bg-center block w-[50%]" style={{ backgroundImage: `url(${ablaze.src})` }}>
             </div> */}
             <div className="md:w-[50%] flex items-center justify-center md:pt-[60px]">
-                <Image src={ablaze} alt="Ablaze" width={500} />
+                <div className="w-full h-full">
+                    <Swiper
+                        modules={[Navigation, Pagination, Autoplay]}
+                        spaceBetween={16}
+                        slidesPerView={1}
+                        navigation
+                        pagination={{ clickable: true }}
+                        autoplay={{ delay: 3500, disableOnInteraction: false }}
+                        loop
+                        className="h-90 w-full rounded-3xl"
+                        aria-live="polite"
+                    >
+                        {[communionsunday, encounternight].map((img, idx) => (
+                            <SwiperSlide key={idx} className="h-90">
+                                <div className="h-90 w-full">
+                                    <Image src={img} alt={`Impact ${idx + 1}`} className="object-cover h-90 w-full" />
+                                </div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </div>
             </div>
         </div>
     )
